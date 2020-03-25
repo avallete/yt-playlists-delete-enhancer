20a1c39f98c25b7864eaebaf9cdf9db00f75799c
termcaps <avallete@student.42.fr>
2020-03-25 20:41:24 +1100

    fix typo on @description

--- yt-watch-later-delete-enhancer.js
+++ yt-watch-later-delete-enhancer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YT Watch Later Delete Enhancer
-// @version      0.1
-// @description:  Add a button to remove videos watched with more than X percent from watch later playlist.
+// @version      0.2
+// @description  Add a button to remove videos watched with more than X percent from watch later playlist.
 // @author       avallete
 // @homepage     https://github.com/avallete/yt-watch-later-delete-enhancer
 // @support      https://github.com/avallete/yt-watch-later-delete-enhancer/issues
