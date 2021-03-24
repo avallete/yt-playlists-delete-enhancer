0273bbb992c1ac723f31ded3dbee1769af75ceb2
avallete <avallete@users.noreply.github.com>
2021-03-24 18:14:37 +0000

    Deploying to gh-pages from @ avallete/yt-playlists-delete-enhancer@679b3baca0e49b105d0a175070d297e485457d5e 🚀

---
+++ yt-playlists-delete-enhancer.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         yt-playlists-delete-enhancer
+// @description  Add a button to remove videos watched with more than X percent from watch later playlist.
+// @version      1.4.2
+// @author       Andrew Valleteau <avallete@student.42.fr>
+// @grant        none
+// @match        *://youtube.com/*
+// @match        *://www.youtube.com/*
+// @namespace    greasyfork-namespace-url
+// @downloadURL  https://raw.githubusercontent.com/avallete/yt-playlists-delete-enhancer/gh-pages/yt-playlists-delete-enhancer.user.js
+// @noframes
+// @homepageURL  https://github.com/avallete/yt-playlists-delete-enhancer#readme
+// @supportURL   https://github.com/avallete/yt-playlists-delete-enhancer/issues
+// @run-at       document-idle
+// @license      MIT
+// ==/UserScript==
