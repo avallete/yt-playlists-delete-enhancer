bdb2d24a73bd3ae6dae11de6fff561da952ceaa8
avallete <avallete@student.42.fr>
2021-03-26 23:05:34 +0100

    fix: inconsistencies on app mount between chrome and firefox

--- src/operations/actions/append-app-to-dom.tsx
+++ src/operations/actions/append-app-to-dom.tsx
@@ -6,7 +6,11 @@ import { YTConfigData } from '~src/youtube'
 export default function appendAppToDom(config: YTConfigData, playlistName: string, xpathRoot: string) {
   const elementToAppendTo = getElementsByXpath(xpathRoot)[0] as Element
   if (elementToAppendTo) {
-    render(<RemoveVideoEnhancerApp config={config} playlistName={playlistName} />, elementToAppendTo)
+    render(
+      <RemoveVideoEnhancerApp config={config} playlistName={playlistName} />,
+      elementToAppendTo,
+      elementToAppendTo.lastElementChild as Element
+    )
   } else {
     throw new Error(`Cannot found ${xpathRoot} in the DOM`)
   }
