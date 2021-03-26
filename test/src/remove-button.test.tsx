cd7fb8048d54d07f3108123639bf8393cc1bf9cd
avallete <avallete@student.42.fr>
2021-03-26 22:29:43 +0100

    refactor: tsx tests fail to compile with ava and preact-material-components

--- test/src/remove-button.test.tsx
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import { render } from '@testing-library/preact'
-import RemoveButton from '~src/components/remove-button'
-
-test('RenderButton contain remove text', (t) => {
-  const { container } = render(<RemoveButton />)
-  t.is(container.textContent, 'Remove !')
-})
