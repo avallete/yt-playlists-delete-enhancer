c2e282e1448f92445751d275c2cbb630d66d9f97
avallete <avallete@student.42.fr>
2021-04-02 11:45:36 +0200

    test: fix e2e tests

--- cypress/integration/simple.spec.ts
+++ cypress/integration/simple.spec.ts
@@ -5,6 +5,11 @@ describe('test extension', () => {
   // TODO: Add more e2e tests when youtube webapp is not a nightmare to work with cypress anymore
   // as today, it's near impossible to do anything close to end user interactions (like login)
   it('extension load and set run attr to <html> tag', () => {
+    // mock youtube website to avoid redirect to 'https://consent.youtube.com'
+    cy.intercept('https://www.youtube.com', {
+      statusCode: 200,
+      body: '<html><body><div>mocked youtube</div></body></html>',
+    })
     cy.visit('https://www.youtube.com')
     cy.get('html').should('have.attr', `data-${U.id}-has-run`)
   })
