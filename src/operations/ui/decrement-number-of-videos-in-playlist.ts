8d51582d411f37c266a09787a1850e3921eb85bd
Dave Nicolson <david.nicolson@gmail.com>
2021-06-01 23:37:59 +0200

    fix: video decrement when playlist contains one video (#104)
    * fix: single video decrement
    
    * fix(typo): grammar

--- src/operations/ui/decrement-number-of-videos-in-playlist.ts
+++ src/operations/ui/decrement-number-of-videos-in-playlist.ts
@@ -3,11 +3,22 @@ import getElementsByXpath from '~src/lib/get-elements-by-xpath'
 
 // Decrement the numbers of videos in the playlist in the UI
 export default function decrementNumberOfVideosInPlaylist(value: number) {
-  const spanElement: HTMLSpanElement = getElementsByXpath(XPATH.YT_NUMBERS_OF_VIDEOS_IN_PLAYLIST)[0] as HTMLSpanElement
+  const spanElement: HTMLSpanElement = getElementsByXpath(XPATH.YT_NUMBER_OF_VIDEOS_IN_PLAYLIST)[0] as HTMLSpanElement
   if (spanElement) {
     const newValue = Number(spanElement.textContent) - value
     spanElement.textContent = `${newValue}`
   } else {
-    throw new Error('span with the number of videos in playlist not found in DOM')
+    const spanElementOneVideo: HTMLSpanElement = getElementsByXpath(
+      XPATH.YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO
+    )[0] as HTMLSpanElement
+    if (spanElementOneVideo) {
+      // A reload is performed to properly restore the state of an empty playlist:
+      // - The "There are no videos in this playlist yet" text
+      // - The "No videos" text
+      // Both strings are not part of the `yt.msgs_` object to use for localization
+      window.location.reload()
+    } else {
+      throw new Error('span with the number of videos in playlist not found in DOM')
+    }
   }
 }
--- src/selectors.ts
+++ src/selectors.ts
@@ -2,7 +2,8 @@ export const XPATH = {
   APP_RENDER_ROOT: '//ytd-playlist-sidebar-renderer/div[@id="items"]/*[last()]',
   YT_PLAYLIST_SIDEBAR_ITEMS: '//ytd-playlist-sidebar-renderer/div[@id="items"]',
   YT_PLAYLIST_VIDEO_RENDERERS: '//ytd-playlist-video-renderer',
-  YT_NUMBERS_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-sidebar-primary-info-renderer/div/yt-formatted-string/span[1]',
+  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-sidebar-primary-info-renderer/div/yt-formatted-string/span[1]',
+  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO: '//ytd-playlist-sidebar-primary-info-renderer/div/yt-formatted-string',
 }
 
 export default {
