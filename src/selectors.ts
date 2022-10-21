ce5b39742db7ba63d371a71220c81794159baf04
Andrew Valleteau <avallete@users.noreply.github.com>
2022-10-21 12:40:44 +0200

    fix: yt selector update (#176)

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,9 +1,8 @@
 export const XPATH = {
-  APP_RENDER_ROOT: '//ytd-playlist-sidebar-renderer/div[@id="items"]/*[last()]',
-  YT_PLAYLIST_SIDEBAR_ITEMS: '//ytd-playlist-sidebar-renderer/div[@id="items"]',
+  APP_RENDER_ROOT: '//div[@class="thumbnail-and-metadata-wrapper style-scope ytd-playlist-header-renderer"]/*[last()]',
   YT_PLAYLIST_VIDEO_RENDERERS: '//ytd-playlist-video-renderer',
-  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-sidebar-primary-info-renderer/div/yt-formatted-string/span[1]',
-  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO: '//ytd-playlist-sidebar-primary-info-renderer/div/yt-formatted-string',
+  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-byline-renderer//div/yt-formatted-string/span[1]',
+  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO: '//ytd-playlist-byline-renderer/div/yt-formatted-string',
 }
 
 export default {
