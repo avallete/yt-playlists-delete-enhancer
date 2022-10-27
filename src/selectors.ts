374f22aca283b025304152dbf93558bb4fbddfaf
Dave Nicolson <david.nicolson@gmail.com>
2022-10-27 06:30:38 +0200

    fix: update Xpath expressions

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,7 +1,7 @@
 export const XPATH = {
-  APP_RENDER_ROOT: '//div[@class="thumbnail-and-metadata-wrapper style-scope ytd-playlist-header-renderer"]/*[last()]',
+  APP_RENDER_ROOT: '//div[@class="style-scope ytd-playlist-sidebar-renderer"]/*[last()]',
   YT_PLAYLIST_VIDEO_RENDERERS: '//ytd-playlist-video-renderer',
-  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-byline-renderer//div/yt-formatted-string/span[1]',
+  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//div[@id="stats"]//yt-formatted-string[1]//span[1]',
 }
 
 export default {
