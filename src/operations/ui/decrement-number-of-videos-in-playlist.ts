10567a978a5721170bbc7f278475b36b9d6eb3fa
Dave Nicolson <david.nicolson@gmail.com>
2022-10-27 06:30:25 +0200

    fix: simplify 1 video playlist behavior

--- src/operations/ui/decrement-number-of-videos-in-playlist.ts
+++ src/operations/ui/decrement-number-of-videos-in-playlist.ts
@@ -8,17 +8,10 @@ export default function decrementNumberOfVideosInPlaylist(value: number) {
     const newValue = Number(spanElement.textContent) - value
     spanElement.textContent = `${newValue}`
   } else {
-    const spanElementOneVideo: HTMLSpanElement = getElementsByXpath(
-      XPATH.YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO
-    )[0] as HTMLSpanElement
-    if (spanElementOneVideo) {
-      // A reload is performed to properly restore the state of an empty playlist:
-      // - The "There are no videos in this playlist yet" text
-      // - The "No videos" text
-      // Both strings are not part of the `yt.msgs_` object to use for localization
-      window.location.reload()
-    } else {
-      throw new Error('span with the number of videos in playlist not found in DOM')
-    }
+    // A reload is performed to properly restore the state of an empty playlist:
+    // - The "There are no videos in this playlist yet" text
+    // - The "No videos" text
+    // Both strings are not part of the `yt.msgs_` object to use for localization
+    window.location.reload()
   }
 }
--- src/selectors.ts
+++ src/selectors.ts
@@ -2,7 +2,6 @@ export const XPATH = {
   APP_RENDER_ROOT: '//div[@class="thumbnail-and-metadata-wrapper style-scope ytd-playlist-header-renderer"]/*[last()]',
   YT_PLAYLIST_VIDEO_RENDERERS: '//ytd-playlist-video-renderer',
   YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-byline-renderer//div/yt-formatted-string/span[1]',
-  YT_NUMBER_OF_VIDEOS_IN_PLAYLIST_ONE_VIDEO: '//ytd-playlist-byline-renderer/div/yt-formatted-string',
 }
 
 export default {
