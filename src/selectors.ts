ed06ffe51204df785700654367a578173f6ac346
avallete <andrew@clovis.pro>
2022-11-17 10:36:47 +0100

    fix(selector): fix app root selector

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,5 +1,5 @@
 export const XPATH = {
-  APP_RENDER_ROOT: '//div[@class="immersive-header-content style-scope ytd-playlist-header-renderer"]',
+  APP_RENDER_ROOT: '//div[@class="style-scope ytd-playlist-sidebar-renderer"]',
   YT_PLAYLIST_VIDEO_RENDERERS: '//ytd-playlist-video-renderer',
   YT_NUMBER_OF_VIDEOS_IN_PLAYLIST: '//ytd-playlist-byline-renderer//div/yt-formatted-string/span[1]',
 }
