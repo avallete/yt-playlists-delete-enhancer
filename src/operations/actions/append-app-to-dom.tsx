8d76158d21821ff264aafb86f0bc829aabfc4c4b
Andrew Valleteau <avallete@users.noreply.github.com>
2022-11-20 00:50:43 +0100

    fix: selector other than watchlater playlist (#199)

--- src/operations/actions/append-app-to-dom.tsx
+++ src/operations/actions/append-app-to-dom.tsx
@@ -2,18 +2,25 @@ import { render } from 'preact'
 import getElementsByXpath from '~lib/get-elements-by-xpath'
 import RemoveVideoEnhancerApp from '~components/remove-video-enhancer-app'
 import { YTConfigData } from '~src/youtube'
+import U from '~src/userscript'
 
 export default function appendAppToDom(config: YTConfigData, playlistName: string, xpathRoot: string) {
-  const elementToAppendTo = getElementsByXpath(xpathRoot)[0] as Element
-  if (elementToAppendTo) {
-    render(
-      // Use Date.now() to force re-mount component to trigger playlist fetch after yt-navigate-finish events
-      // See: #62
-      <RemoveVideoEnhancerApp key={Date.now()} config={config} playlistName={playlistName} />,
-      elementToAppendTo,
-      elementToAppendTo.lastElementChild as Element
+  const existingElement = document.querySelector(`#${U.id}${playlistName}`)
+  if (!existingElement) {
+    const elementToAppendTo = getElementsByXpath(xpathRoot)
+      // get only currently visibles elements because youtube hide elements instead of removing them from the DOM
+      .find((element: any) => element.offsetHeight > 0 || element.offsetWidth > 0) as Element
+    // Use Date.now() to force re-mount component to trigger playlist fetch after yt-navigate-finish events
+    // See: #62
+    const AppContainer = (
+      <div id={`${U.id}${playlistName}`} key={Date.now()}>
+        <RemoveVideoEnhancerApp key={Date.now()} config={config} playlistName={playlistName} />
+      </div>
     )
-  } else {
-    throw new Error(`Cannot found ${xpathRoot} in the DOM`)
+    if (elementToAppendTo) {
+      render(AppContainer, elementToAppendTo, elementToAppendTo.lastElementChild as Element)
+    } else {
+      throw new Error(`Cannot found ${xpathRoot} in the DOM`)
+    }
   }
 }
